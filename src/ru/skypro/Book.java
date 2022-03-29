04732cf51679d4ba09e4e126e86a3d823f0f59d3
Tony <antutd@yandex.ru>
2022-03-30 03:31:16 +0500

    Сделал Дз

--- src/ru/skypro/Book.java
+++ src/ru/skypro/Book.java
@@ -1,4 +1,55 @@
 package ru.skypro;
+class Author {
+    String name;
+    String surname;
+
+    public Author (String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String GetNameAuthor() {
+        return name;
+    }
+    public String GetSurnameAuthor() {
+        return surname;
+    }
+
+    }
 
 public class Book {
+    String author;
+    static String bookName;
+    static int publicationYear;
+    static String surname;
+    static String name;
+    String thisBookName;
+
+    public Book(String bookName, int publicationYear, String author) {
+        this.author = author;
+        this.bookName = bookName;
+        this.publicationYear = publicationYear;
+        this.surname = surname;
+        this.name = name;
+    }
+
+    public String GetBookName() {
+        return bookName;
+    }
+
+    public int GetPublicationYear() {
+        return publicationYear;
+
+    }
+
+    public void setpublicationYear(int publicationYear) { // сеттер года публикации
+        this.publicationYear = publicationYear;
+
+    }
+
 }
+
+
+
+
+
